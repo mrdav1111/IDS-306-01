667dc9a244a57394b9cbc31632d42894eb268f67
Oscarben110 <oscarben110@gmail.com>
2017-04-20 17:13:11 -0400

    Look

---
+++ Agenda/Models/atributos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Agenda.Models
+{
+    public class atributos
+    {
+        public int Telefono { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+        
+    }
+
+    public class atributosDBContext : DbContext
+    {
+        public DbSet<atributos> Agenda { get; set; }
+    }
+
+}
