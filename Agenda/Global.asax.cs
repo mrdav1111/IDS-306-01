079d6425b7db78f6f93a8ee3ce7ec7e042def627
Angel <angeldavidn13@gmail.com>
2017-04-20 18:08:45 -0400

    Aqui funciona

--- Agenda/Global.asax.cs
+++ Agenda/Global.asax.cs
@@ -13,7 +13,7 @@ namespace Agenda
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+           
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
